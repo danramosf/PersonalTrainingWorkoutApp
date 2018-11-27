e87ed3cd57cc94262a75ff8e7e2fa126e6463720
danramosf <danramosf@gmail.com>
2018-11-27 12:06:49 -0500

    Initial page added

---
+++ WorkOutApp/StartingForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkOutApp
+{
+    public partial class StartingForm : Form
+    {
+        public StartingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUserPanel_Click(object sender, EventArgs e)
+        {
+            Workout_Session workoutSessionForm = new Workout_Session();
+            workoutSessionForm.Show();
+        }
+    }
+}
